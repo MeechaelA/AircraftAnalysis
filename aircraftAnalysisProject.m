80588e68d43b438e1fb99d74387377759d2a58d5
Brady Burnsides <brady@burnsides.us>
2018-11-10 21:33:33 -0600

    Add files via upload
    Thrust available and thrust required at SSL plotted and max velocity determined

--- aircraftAnalysisProject.m
+++ aircraftAnalysisProject.m
@@ -1,4 +1,4 @@
-clear, clc, format long
+hold off, close all, clear, clc, format long
 % Hydra X6 attack bomber
 
 % Test flight 1 - Max velocity data
@@ -23,12 +23,12 @@ clear, clc, format long
 % (3) Rate of climb vs velocity at sea level
 
 % Table with info requested by Fury (range, endurance, ceiling)
-% 1) Whatï¿½s their maximum range, and at what velocity and altitude?
-% 2) Whatï¿½s their maximum endurance, and at what velocity and altitude?
+% 1) What’s their maximum range, and at what velocity and altitude?
+% 2) What’s their maximum endurance, and at what velocity and altitude?
 % 3) The helicarrier has an operating altitude of 15,000 ft. If these aircrafts engage the
-% helicarrier, whatï¿½s the max duration could they operate?
-% 4) Whatï¿½s their best absolute and service ceiling? And at what velocities?
-% 5) Whatï¿½s their max rate of climb at sea level?
+% helicarrier, what’s the max duration could they operate?
+% 4) What’s their best absolute and service ceiling? And at what velocities?
+% 5) What’s their max rate of climb at sea level?
 %% General Calculations
 W = 18000; % [lb] THIS IS FULLY FUELED WEIGHT
 span = 50.87; % Tip2tip span [ft]
@@ -37,15 +37,21 @@ AR = span^2/S; % Aspect Ratio [ft^2/ft^2]
 e = 1.78*(1-(0.045*AR^(0.68)))-0.64;
 
 % atmospheric conditions
-rhoSL  = 2.3769*10^-3;  % [slugs/ft^3]
+rhoSL  = 2.377*10^-3;  % [slugs/ft^3]
 rho15k = 1.4962*10^-3; % [slugs/ft^3]
 rho20k = 1.2673*10^-3; % [slugs/ft^3]
 
-T20k = 447.43; % [degR]
-%% Test Flight 2 Calcs
+Temp20k = 447.43; % [degR]
+
+% repeated functions
+%% Test Flight 1 Calcs (Max Vel)
+MTest1 = 0.68;
+a20k = sqrt(1.4*1716*Temp20k);
+V20kTest1 = MTest1*a20k;
+
+%% Test Flight 2 Calcs (Max Endurance)
 
 MTest2 = 0.3233;
-a20k = sqrt(1.4*1716*T20k);
 V20kTest2 = MTest2*a20k;
 Cl20k = W/(0.5*rho20k*V20kTest2^2*S);
 Cd20k = Cl20k/15.331;
@@ -54,14 +60,33 @@ Cd0 = Cd20k - Cdi20k;
 
 %% Plots
 % (1) Tr and Ta vs velocity at sea level
+
+% 20k - use max velocity at 20k to find max velocity at SL (needed to find Ta)
+figure(1)
+Tr20k = @(v) W./((W./(((.5)*rho20k*(v.^2)) * S))./(Cd0 + (((W./(((.5)*rho20k*(v.^2)) * S)).^2)/(pi*e*AR))));
+fplot(Tr20k, [150 900]), title('Thrust Required and Thrust Available vs Velocity at 20,000 [ft]')
+xlabel('Velocity [ft/s]'), ylabel('Thrust [lb_f]'), xlim([0 900]), ylim([0 6000]), grid on, hold on
+Ta20k = Tr20k(V20kTest1);
+fplot(@(V) Ta20k+(0.*V), [0 900]); plot(V20kTest1, Ta20k, 'r*')
+legend('Thrust Required', 'Thrust Available', 'Maximum Velocity at 20,000 [ft]','Location', 'southeast'), hold off
+TaSL = Ta20k*(rhoSL/rho20k);
+TaSLTable = @(V) TaSL + (0.*V);
 % SL
-% ClSL = W/(.5*rhoSL*V^2*S)
-% CdSL = Cd0 + ((ClSL^2)/(pi*e*AR));
+figure(2)
+TrSL = @(v) W./((W./(((.5)*rhoSL*(v.^2)) * S))./(Cd0 + (((W./(((.5)*rhoSL*(v.^2)) * S)).^2)/(pi*e*AR))));
+fplot(TrSL, [150 900]), title('Thrust Required and Thrust Available vs Velocity at Sea Level')
+xlabel('Velocity [ft/s]'), ylabel('Thrust [lb_f]'), xlim([0 900]), ylim([0 6000]), grid on, hold on
+fplot(@(V) TaSL+(0.*V), [0 900])
+SLdataPoint = [722.78:.001:722.79; TaSLTable(722.78:.001:722.79); TrSL(722.78:.001:722.79);]'
+plot(722.789, TrSL(722.789), 'r*')
+legend('Thrust Required', 'Thrust Available', 'Maximum Velocity at Sea Level','Location', 'southeast'), hold off
 
-TrSL = @(V) W/((W/(.5*rhoSL*V^2*S))/(Cd0 + (((W/(.5*rhoSL*V^2*S))^2)/(pi*e*AR))));
-fplot(TrSL, [0 400], 'b-'), xlim([0 350])
 % (2) Pr and Pa vs velocity at sea level, 15,000 ft, and 20,000 ft
-
+figure(3)
+PrSL = @(V) TrSL(V)*V;
+PaSL = @(V) TaSL*V;
+fplot(PrSL, [60 850]), hold on, grid on
+fplot(PaSL, [0 850])
 %% Questions and table data
 
 % 1) What's their maximum range, and at what velocity and altitude?
