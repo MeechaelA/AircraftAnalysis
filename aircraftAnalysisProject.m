98b6f1a486e030038e956bbca6d37471f26eb1c2
Brady Burnsides <brady@burnsides.us>
2018-11-10 22:26:59 -0600

    Add files via upload
    Cleaned up plots and added Rate of Climb at SSL plot. Max RC marked.

--- aircraftAnalysisProject.m
+++ aircraftAnalysisProject.m
@@ -60,55 +60,59 @@ Cd0 = Cd20k - Cdi20k;
 
 %% Plots
 % (1) Tr and Ta vs velocity at sea level
-
-% 20k - use max velocity at 20k to find max velocity at SL (needed to find Ta)
-figure(1)
 Tr20k = @(V) W./((W./(((.5)*rho20k*(V.^2)) * S))./(Cd0 + (((W./(((.5)*rho20k*(V.^2)) * S)).^2)/(pi*e*AR))));
-fplot(Tr20k, [150 900]), title('Thrust Required and Thrust Available vs Velocity at 20,000 [ft]')
-xlabel('Velocity [ft/s]'), ylabel('Thrust [lb_f]'), xlim([0 900]), ylim([0 6000]), grid on, hold on
 Ta20k = Tr20k(V20kTest1);
-fplot(@(V) Ta20k+(0.*V), [0 900]); plot(V20kTest1, Ta20k, 'r*')
-legend('Thrust Required', 'Thrust Available', 'Maximum Velocity at 20,000 [ft]','Location', 'southeast'), hold off
 TaSL = Ta20k*(rhoSL/rho20k);
 TaSLTable = @(V) TaSL + (0.*V);
 % SL
-figure(2)
+figure(1)
 TrSL = @(V) W./((W./(((.5)*rhoSL*(V.^2)) * S))./(Cd0 + (((W./(((.5)*rhoSL*(V.^2)) * S)).^2)/(pi*e*AR))));
 fplot(TrSL, [150 900]), title('Thrust Required and Thrust Available vs Velocity at Sea Level')
 xlabel('Velocity [ft/s]'), ylabel('Thrust [lb_f]'), xlim([0 900]), ylim([0 6000]), grid on, hold on
 fplot(@(V) TaSL+(0.*V), [0 900])
-SLdataPoint = [722.78:.001:722.79; TaSLTable(722.78:.001:722.79); TrSL(722.78:.001:722.79);]'
+ThrustSLdataPoints = [722.78:.001:722.79; TaSLTable(722.78:.001:722.79); TrSL(722.78:.001:722.79);]';
 plot(722.789, TrSL(722.789), 'r*')
 legend('Thrust Required', 'Thrust Available', 'Maximum Velocity at Sea Level','Location', 'southeast'), hold off
 
 % (2) Pr and Pa vs velocity at sea level, 15,000 ft, and 20,000 ft
 % SL
-figure(3)
-PrSL = @(V) TrSL(V)*V*0.001818;
-PaSL = @(V) TaSL*V*0.001818;
-fplot(PrSL, [60 775]), hold on, grid on
-fplot(PaSL, [0 775]), xlabel('Velocity [ft/s]'), ylabel('Power [hp]')
+figure(2)
+PrSL = @(V) TrSL(V).*V;
+PaSL = @(V) TaSL.*V;
+fplot(@(V) PrSL(V).*0.001818, [60 775]), hold on, grid on
+fplot(@(V) PaSL(V).*0.001818, [0 775]), xlabel('Velocity [ft/s]'), ylabel('Power [hp]')
 title('Power Requred and Power Available vs Velocity at Sea Level')
 legend('Power Required', 'Power Available', 'Location', 'southeast'), hold off
 % 15k
-figure(4)
-Tr15k = @(V) W./((W./(((.5)*rho15k*(V.^2)) * S))./(Cd0 + (((W./(((.5)*rho15k*(V.^2)) * S)).^2)/(pi*e*AR))));
-Ta15k = Ta20k*(rho15k/rho20k);
-Pr15k = @(V) Tr15k(V)*V*0.001818;
-Pa15k = @(V) Ta15k*V*0.001818;
-fplot(Pr15k, [60 775]), hold on, grid on
-fplot(Pa15k, [0 775]), xlabel('Velocity [ft/s]'), ylabel('Power [hp]')
+figure(3)
+Tr15k = @(V) W./((W./(((.5).*rho15k.*(V.^2)) .* S))./(Cd0 + (((W./(((.5).*rho15k.*(V.^2)) .* S)).^2)./(pi.*e.*AR))));
+Ta15k = Ta20k.*(rho15k./rho20k);
+Pr15k = @(V) Tr15k(V).*V;
+Pa15k = @(V) Ta15k.*V;
+fplot(@(V) Pr15k(V).*0.001818, [110 775]), hold on, grid on
+fplot(@(V) Pa15k(V).*0.001818, [0 775]), xlabel('Velocity [ft/s]'), ylabel('Power [hp]')
 title('Power Requred and Power Available vs Velocity at 15,000 [ft]')
 legend('Power Required', 'Power Available', 'Location', 'southeast'), hold off
 % 20k
-figure(5)
-Tr20k = @(V) W./((W./(((.5)*rho20k*(V.^2)) * S))./(Cd0 + (((W./(((.5)*rho20k*(V.^2)) * S)).^2)/(pi*e*AR))));
-Pr20k = @(V) Tr20k(V)*V*0.001818;
-Pa20k = @(V) Ta20k*V*0.001818;
-fplot(Pr20k, [60 775]), hold on, grid on
-fplot(Pa20k, [0 775]), xlabel('Velocity [ft/s]'), ylabel('Power [hp]')
+figure(4)
+Tr20k = @(V) W./((W./(((.5).*rho20k.*(V.^2)) .* S))./(Cd0 + (((W./(((.5).*rho20k.*(V.^2)) .* S)).^2)/(pi.*e.*AR))));
+Pr20k = @(V) Tr20k(V).*V;
+Pa20k = @(V) Ta20k.*V;
+fplot(@(V) Pr20k(V).*0.001818, [125 775]), hold on, grid on
+fplot(@(V) Pa20k(V).*0.001818, [0 775]), xlabel('Velocity [ft/s]'), ylabel('Power [hp]')
 title('Power Requred and Power Available vs Velocity at 20,000 [ft]')
 legend('Power Required', 'Power Available', 'Location', 'southeast'), hold off
+
+% (3) Rate of climb vs velocity at sea level
+% RC = (Pa - Pr)/W
+figure(5)
+RCSL = @(V) (PaSL(V)-PrSL(V))/W;
+fplot(RCSL, [0 800]), ylim([0 100]), grid on, hold on
+xlabel('Velocity [ft/s]'), ylabel('Rate of Climb [ft/s]')
+title('Rate of Climb at Sea Level')
+plot(4.4088*10^2, 0.6323*10^2, 'r*')
+legend('Rate of Climb at Sea Level', 'Maximum Rate of Climb at Sea Level (63.23 [ft/s])')
+RCSLdataPoints = [400:.01:450; RCSL(400:.01:450)]';
 %% Questions and table data
 
 % 1) What's their maximum range, and at what velocity and altitude?
